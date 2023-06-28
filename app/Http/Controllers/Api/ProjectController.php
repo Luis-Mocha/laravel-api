b3eb555f9fc3393bb73b5ec081c05fbc36c7ef19
Luis-Mocha <luismocha97@gmail.com>
2023-06-28 18:48:59 +0200

    funzione con "with"

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,9 @@ use App\Models\Admin\Project;
 class ProjectController extends Controller
 {
     public function index() {
-        $projects = Project::all();
+        // $projects = Project::all();
+
+        $projects = Project::with('type', 'technologies')->get();
 
         return response()->json([
             'success' => true,
